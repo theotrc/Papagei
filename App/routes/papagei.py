17216752ce2413e5d0efb2f6e0d36a335707caf8
theo <theotricot12@gmail.com>
2023-04-25 16:58:04 +0200

    papagei page

---
+++ App/routes/papagei.py
@@ -0,0 +1,11 @@
+from flask import render_template, Blueprint
+
+
+
+papagei_blue= Blueprint("papagei", __name__, static_folder='../static', template_folder='templates')
+
+
+@papagei_blue.route("/")
+def papagei():
+    
+    return render_template("papagei.html")
