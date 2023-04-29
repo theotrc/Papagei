da97f7e6ad69c74d1a50b743559153ead6006329
theotrc <theotricot12@gmail.com>
2023-04-29 17:26:48 +0200

    'front'

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:27L54ch[cE+{PD7?@35.205.61.54:5432/papagei-sql'
 SECRET_KEY = os.environ.get('SECRET')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 UPLOAD_FOLDER = os.environ.get('FOLDER_PATH')
