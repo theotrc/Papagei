2853433c125b055732e488b67f4b1d1c9ea51a28
theotrc <theotricot12@gmail.com>
2023-05-30 09:01:21 +0200

    starting stripe tuto

--- App/routes/checkout.py
+++ App/routes/checkout.py
@@ -9,6 +9,14 @@ checkout_blue= Blueprint("checkout", __name__, static_folder="../static", templa
 
 @checkout_blue.route('/checkout')
 def checkout():
+    
+    session = stripe.checkout.Session.create(
+        payement_method_types=["card"],
+        line_items=[{"price":10, "quantity":1}], 
+        mode="payement", 
+        success_url="https://example.com/success?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url="https://example.com/cancel",
+    )
 
     return render_template("checkout.html")
 
