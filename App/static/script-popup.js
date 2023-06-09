e54859f9481c4678ce80edfe48026eba6c0e6a8f
Louise Petit <louisepetit@mbp-de-louise-3.home>
2023-06-09 23:09:27 +0200

    basket

---
+++ App/static/script-popup.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.
+            display ="block";
+        },
+        1100
+    )
+});
+
+document.querySelector("#close").addEventListener
+("click", function(){
+    console.log("closndejzfjezbfjk")
+    document.querySelector("#popup"). 
+    getElementsByClassName.display ="none";
+});
