60435b20db281b57e7f4fd66112c0f8856641350
theotrc <theotricot12@gmail.com>
2023-06-11 19:37:09 +0200

    bug

--- App/models.py
+++ App/models.py
@@ -57,7 +57,7 @@ class Item(db.Model):
 
     stock = db.Column(db.Integer)
 
-    about_model = color= db.Column(db.String(1000))
+    about_model = db.Column(db.String(1000))
 
     quantity =db.Column(db.Integer)
 
