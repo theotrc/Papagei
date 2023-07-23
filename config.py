25b69fd9a4d1bfa8b896484971922b1bfc492fb0
theotrc <theotricot12@gmail.com>
2023-07-23 20:56:08 +0200

    changebdd

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = os.environ.get('GCP')
+SQLALCHEMY_DATABASE_URI = os.environ.get('OVHDB')
 SECRET_KEY = os.environ.get('SECRET')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 UPLOAD_FOLDER = os.environ.get('FOLDER_PATH')
