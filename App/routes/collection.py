405e41db51011d0dd745ff163f24ebd666d991b3
theotrc <theotricot12@gmail.com>
2023-11-14 22:15:01 +0100

    collection

--- App/routes/collection.py
+++ App/routes/collection.py
@@ -1,7 +1,9 @@
+from re import template
 from flask import render_template, Blueprint, request,redirect, url_for
 from ..models import Collection
 from flask_login import login_required,current_user
 from App import db
+from ..models import Item, Collection
 
 collection_blue= Blueprint("collection", __name__, static_folder='../static', template_folder='../templates')
 
@@ -12,6 +14,10 @@ collection_blue= Blueprint("collection", __name__, static_folder='../static', te
 @collection_blue.route('/collections')
 @login_required
 def collections():
+    """ 
+    Manage collection (admin)
+    
+    """
     if current_user.is_admin:
         items = Collection.query.all()
         return render_template("collections.html", items=items)
@@ -21,6 +27,10 @@ def collections():
 @collection_blue.route('/newcollection')
 @login_required
 def new():
+    """ 
+    Create new collection (admin)
+    
+    """
     if current_user.is_admin:
         return render_template("form_collection.html")
     else:
@@ -29,6 +39,10 @@ def new():
 @collection_blue.route('/newcollection', methods=["POST"])
 @login_required
 def new_post():
+    """ 
+    Create new collection (admin) POST
+    
+    """
     if current_user.is_admin:
         name = request.form.get('name')
         new_collection = Collection(
@@ -49,6 +63,10 @@ def new_post():
 @collection_blue.route('/deleteCollection<id>', methods=["POST"])
 @login_required
 def delete_collection(id):
+    """ 
+    Delete collection (admin) POST
+    
+    """
     if current_user.is_admin:
         print(id)
         # name = request.form.get('name')
@@ -63,5 +81,39 @@ def delete_collection(id):
         
         return redirect(url_for("collection.collections"))
     
+    else:
+        return redirect(url_for("home.home"))
+    
+    
+@collection_blue.route('/additemcollection<collectionId>')
+@login_required
+def add_item_collection(collectionId):
+    """ 
+    Add item to a collection (admin)
+    
+    """
+    if current_user.is_admin:
+        
+        item = Item.query.order_by(Item.sort).all()  
+        
+        return render_template("add_items_collections.html", items = item, byte = bytes(),collectionId=collectionId)
+    else:
+        return redirect(url_for("home.home"))
+    
+@collection_blue.route('/additemcollection/<itemId>/<collectionId>', methods=["POST"])
+@login_required
+def add_item_collection_post(collectionId,itemId):
+    """ 
+    Add item to a collection (admin)
+    
+    """
+    if current_user.is_admin:
+        print("collection: ",collectionId)
+        print("article: ", itemId)
+        item = Item.query.all()
+        print("collection",item[0].collection)
+        
+        
+        return redirect(url_for("collection.add_item_collection", collectionId=collectionId))
     else:
         return redirect(url_for("home.home"))
