5b7e8e2b1c2230521685b40ad65637735213efa0
theotrc <theotricot12@gmail.com>
2023-11-21 20:39:50 +0100

    new routes

--- App/routes/mention.py
+++ App/routes/mention.py
@@ -23,3 +23,12 @@ def cgv():
 
     
     return render_template("cgv.html")
+
+
+@mention_blue.route("/about")
+def about():
+    return render_template("about.html")
+
+@mention_blue.route("/ecologialcommitment")
+def ecologial_commitment():
+    return render_template("ecologial_commitment.html")
